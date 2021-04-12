3be5e6893074766d3d30f62d421afef7d8f9b901
Peter Butkovic <peter.butkovic@sk.ibm.com>
2021-04-12 23:30:58 +0200

    chore: ci/cd fix (no. 7)

--- k8s/create-deploy-svc.sh
+++ k8s/create-deploy-svc.sh
@@ -7,10 +7,10 @@ kubectl create deployment ${DEPLOYMENT_NAME} --image=${DOCKER_IMG} --dry-run=cli
 kubectl set env deployment/${DEPLOYMENT_NAME} --from configmap/${DEPLOYMENT_NAME} --dry-run=client -o yaml \
   > ${DEPLOYMENT_NAME}-deploy-cm.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-deploy-cm.yaml
 
-if [[ -z "${SVC_NODEPORT}" ]]; then
-  kubectl create service $SVC_TYPE ${DEPLOYMENT_NAME} --tcp=$SVC_PORT:$SVC_PORT --dry-run=client -o yaml \
+if [ "${SVC_TYPE}" == "nodeport" ]]; then
+  kubectl create service $SVC_TYPE ${DEPLOYMENT_NAME} --node-port=$SVC_NODEPORT --tcp=$SVC_PORT:$SVC_PORT --dry-run=client -o yaml \
   > ${DEPLOYMENT_NAME}-svc.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-svc.yaml
 else
-  kubectl create service $SVC_TYPE ${DEPLOYMENT_NAME} --node-port=$SVC_NODEPORT --tcp=$SVC_PORT:$SVC_PORT --dry-run=client -o yaml \
+  kubectl create service $SVC_TYPE ${DEPLOYMENT_NAME} --tcp=$SVC_PORT:$SVC_PORT --dry-run=client -o yaml \
   > ${DEPLOYMENT_NAME}-svc.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-svc.yaml
 fi

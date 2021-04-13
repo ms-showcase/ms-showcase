f26763993ccd4991d098b55d5dcef094bc1ac176
Peter Butkovic <peter.butkovic@sk.ibm.com>
2021-04-13 06:58:56 +0200

    chore: k8s cm for non-empty props only

--- k8s/create-deploy-svc.sh
+++ k8s/create-deploy-svc.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e
 
+# if prop file not empty
+if [ -s k8s/${DEPLOYMENT_NAME}.properties ]; then
 kubectl create configmap ${DEPLOYMENT_NAME} --from-env-file=k8s/${DEPLOYMENT_NAME}.properties --dry-run=client -o yaml \
   > ${DEPLOYMENT_NAME}-cm.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-cm.yaml
+fi
+
 kubectl create deployment ${DEPLOYMENT_NAME} --image=${DOCKER_IMG} --dry-run=client -o yaml \
   > ${DEPLOYMENT_NAME}-deploy.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-deploy.yaml
+
+# if prop file not empty
+if [ -s k8s/${DEPLOYMENT_NAME}.properties ]; then
 kubectl set env deployment/${DEPLOYMENT_NAME} --from configmap/${DEPLOYMENT_NAME} --dry-run=client -o yaml \
   > ${DEPLOYMENT_NAME}-deploy-cm.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-deploy-cm.yaml
+fi
 
 if [ "${SVC_TYPE}" == "nodeport" ]; then
   kubectl create service $SVC_TYPE ${DEPLOYMENT_NAME} --node-port=$SVC_NODEPORT --tcp=$SVC_PORT:$SVC_PORT --dry-run=client -o yaml \

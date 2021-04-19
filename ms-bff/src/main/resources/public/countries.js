856be2da8ff8b9020c946639409c5bd8796bcab9
Adam Seidel <aseidel@sk.ibm.com>
2021-04-19 16:04:12 +0200

    Remove redundant code

--- ms-bff/src/main/resources/public/countries.js
+++ ms-bff/src/main/resources/public/countries.js
@@ -7,12 +7,9 @@ $(document).ready(function () {
 
     const url = 'http://127.0.0.1:8998/data/isocodes';
 
-    // Populate dropdown with list of provinces
     $.getJSON(url, function (data) {
         $.each(data, function (key, entry) {
-            // var listItem = document.createElement('li');
-            // var dropdownitem = listItem.append($('<a class="dropdown-item" href="#"></a>').text(entry));
-            // dropdown.append(dropdownitem);
+
             dropdown.append(
                 $('<li/>').append(
                     $('<a/>', {'class': 'dropdown-item'}).text(entry)

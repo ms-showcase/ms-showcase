2ed02a930213917ac7a566eb0beba96538afbc6d
Peter Butkovic <peter.butkovic@sk.ibm.com>
2021-04-19 22:29:05 +0200

    fix: rabbitmq queue auto-creation (try #4)

--- k8s/rabbitmq/create-rabbitmq-config.sh
+++ k8s/rabbitmq/create-rabbitmq-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-kubectl create configmap rabbitmq-config --from-file k8s/rabbitmq/definitions.json --from-file k8s/rabbitmq/rabbitmq.conf
-kubectl patch deployment ${DEPLOYMENT_NAME} --patch k8s/rabbitmq/patch-rabbitmq-volume.yml
+kubectl create configmap rabbitmq-config --from-file k8s/rabbitmq/definitions.json --from-file k8s/rabbitmq/rabbitmq.conf --dry-run=client -o yaml \
+  > ${DEPLOYMENT_NAME}-rabbitmq-config.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-rabbitmq-config.yaml
+cat k8s/rabbitmq/patch-rabbitmq-volume.yml | k8s/mo | kubectl patch deployment ${DEPLOYMENT_NAME} --patch "$(cat)"

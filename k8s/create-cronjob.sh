4a293dcb67ad2cce1f293bf52c1d391cad0821aa
Peter Butkovic <peter.butkovic@sk.ibm.com>
2021-04-20 21:38:52 +0200

    feat: ms-covid-data-loader immediate run

--- k8s/create-cronjob.sh
+++ k8s/create-cronjob.sh
@@ -8,3 +8,6 @@ kubectl create cronjob ${DEPLOYMENT_NAME} --image=${DOCKER_IMG} --schedule="0 8 
   > ${DEPLOYMENT_NAME}-cronjob.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-cronjob.yaml
 kubectl set env cronjob/${DEPLOYMENT_NAME} --from configmap/${DEPLOYMENT_NAME} --dry-run=client -o yaml \
   > ${DEPLOYMENT_NAME}-cronjob-cm.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-cronjob-cm.yaml
+
+kubectl create job --from=cronjob/ms-covid-data-loader ms-convid-data-loader-on-demand --dry-run=client -o yaml \
+  > ${DEPLOYMENT_NAME}-job.yaml && kubectl apply -f ${DEPLOYMENT_NAME}-job.yaml

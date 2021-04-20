f9c27af2bb0496c651983f166e6d6fd0bd0bd6e0
Peter Butkovic <peter.butkovic@sk.ibm.com>
2021-04-20 15:56:26 +0200

    feat: ms-population jaeger integration

--- k8s/run-local.sh
+++ k8s/run-local.sh
@@ -6,7 +6,13 @@ compose="-f docker-compose.yml"
 for ms in "$@"
 do
     echo "$ms"
-    pushd "$ms"; gradle build ;  docker build -t msshowcase/"$ms" .; popd;
+    pushd "$ms"; gradle build
+    if  [ "$ms" != "ms-population" ]; then
+      docker build -t msshowcase/"$ms" .; popd;
+    else
+      docker build -t msshowcase/"$ms" -f src/main/docker/Dockerfile.jvm .; popd;
+    fi
+
     if  [ "$ms" != "ms-cloud-config" ]; then
       compose="${compose} -f docker-compose-${ms}.yml"
     fi

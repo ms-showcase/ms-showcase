a7337ae73f96f8032ec12cba89ed4ba18a2815d5
Peter Butkovic <peter.butkovic@sk.ibm.com>
2021-04-21 09:31:42 +0200

    feat: introduced CORS headers for https://ms-showcase.github.io in ms-bff

--- ms-bff/src/main/java/io/msdemo/controller/CovidDataController.java
+++ ms-bff/src/main/java/io/msdemo/controller/CovidDataController.java
@@ -3,6 +3,7 @@ package io.msdemo.controller;
 import io.msdemo.feign.CovidDataFeignService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,21 +16,26 @@ public class CovidDataController {
     public CovidDataController(CovidDataFeignService feignService) {
         this.feignService = feignService;
     }
+
+    @CrossOrigin(origins = "https://ms-showcase.github.io")
     @GetMapping(path = "/data/{id}")
     public ResponseEntity<String> findById(@PathVariable("id") String id){
         return new ResponseEntity<>(feignService.findById(id), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "https://ms-showcase.github.io")
     @GetMapping(path = "data/lastweek/{iso}/{date}")
     public ResponseEntity<String> findWeekOldRecords(@PathVariable("iso") final String iso,@PathVariable("date") final String date){
         return new ResponseEntity<>(feignService.findWeekOldRecords(iso, date), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "https://ms-showcase.github.io")
     @GetMapping(path = "/data/isocodes")
     public ResponseEntity<String> isoCodes(){
         return new ResponseEntity<>(feignService.isoCodes(), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "https://ms-showcase.github.io")
     @GetMapping(path = "/data/statistics/{iso}")
     public ResponseEntity<String> lastYearStatistics(@PathVariable("iso") final String iso){
         return new ResponseEntity<>(feignService.lastYearStatistics(iso), HttpStatus.OK);

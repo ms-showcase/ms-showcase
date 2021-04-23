bf9d516a6bd88d72cad2cbee3d83594e7f93af31
Adam Seidel <aseidel@sk.ibm.com>
2021-04-23 13:51:23 +0200

    Revert timelimiter

--- ms-bff/src/main/java/io/msdemo/controller/PopulationController.java
+++ ms-bff/src/main/java/io/msdemo/controller/PopulationController.java
@@ -1,7 +1,5 @@
 package io.msdemo.controller;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import io.msdemo.dto.PopulationDto;
 import io.msdemo.feign.PopulationFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,20 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PopulationController {
+@RestController public class PopulationController {
 
     private PopulationFeignService populationFeignService;
 
-    @Autowired
-    public PopulationController(PopulationFeignService populationFeignService) {
+    @Autowired public PopulationController(PopulationFeignService populationFeignService) {
         this.populationFeignService = populationFeignService;
     }
 
     @GetMapping(path = "/population/{iso}")
-    public ResponseEntity<PopulationDto> population(@PathVariable("iso") final String iso)
-        throws ExecutionException, InterruptedException {
-        return new ResponseEntity<>(populationFeignService.population(iso).get(), HttpStatus.OK);
+    public ResponseEntity<PopulationDto> population(@PathVariable("iso") final String iso) {
+        return new ResponseEntity<>(populationFeignService.population(iso), HttpStatus.OK);
     }
 }
 
--- ms-bff/src/main/java/io/msdemo/feign/PopulationFeignService.java
+++ ms-bff/src/main/java/io/msdemo/feign/PopulationFeignService.java
@@ -1,9 +1,6 @@
 package io.msdemo.feign;
 
-import java.util.concurrent.CompletableFuture;
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import io.msdemo.dto.PopulationDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
@@ -11,18 +8,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "ms-population", url = "${client.url.ms-population:http://127.0.0.1:8091}",
-    configuration = FeignClientsConfiguration.class)
+@FeignClient(name = "ms-population", url = "${client.url.ms-population:http://127.0.0.1:8191}", configuration = FeignClientsConfiguration.class)
 public interface PopulationFeignService {
 
     @RequestMapping(value = "population/{iso}", method = RequestMethod.GET)
     @CircuitBreaker(name = "ms-population", fallbackMethod = "populationFallback")
-    @Bulkhead(name = "ms-population", type = Bulkhead.Type.THREADPOOL)
-    @TimeLimiter(name = "ms-population")
-    CompletableFuture<PopulationDto> population(@PathVariable("iso") String iso);
+    PopulationDto population(@PathVariable("iso") String iso);
 
-    default CompletableFuture<PopulationDto> populationFallback(Throwable throwable){
-        return CompletableFuture.completedFuture(PopulationDto.builder().country("N/A").countryCode("N/A").population("N/A").build());
+    default PopulationDto populationFallback(Throwable throwable){
+        return PopulationDto.builder().country("N/A").countryCode("N/A").population("N/A").build();
     }
 
 }

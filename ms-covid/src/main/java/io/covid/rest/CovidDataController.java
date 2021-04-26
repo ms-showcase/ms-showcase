29ecaa1eb5aecce233c8930ac07736432709419b
Adam Seidel <aseidel@sk.ibm.com>
2021-04-26 23:10:51 +0200

    Swagger OpenApi for ms-covid

--- ms-covid/src/main/java/io/covid/rest/CovidDataController.java
+++ ms-covid/src/main/java/io/covid/rest/CovidDataController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import io.covid.db.model.CovidData;
 import io.covid.dto.chart.Series;
 import io.covid.service.CovidDataService;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,30 +24,38 @@ public class CovidDataController {
         this.covidDataService = covidDataService;
     }
 
-    @GetMapping(path = "/data/{id}")
-    public ResponseEntity<CovidData> findById(@PathVariable("id") final String id){
+    @Operation(summary = "Get CovidData by its id") @GetMapping(path = "/data/{id}")
+    public ResponseEntity<CovidData> findById(
+        @ApiParam(name = "ID of CovidData") @PathVariable("id") final String id) {
         Optional<CovidData> covidData = covidDataService.searchByCovidDataId(id);
         return covidData.map(data -> new ResponseEntity<>(data, HttpStatus.OK))
             .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 
+    @Operation(summary = "Get last week CovidData for a specified country")
     @GetMapping(path = "/data/lastweek/{iso}/{date}")
-    public ResponseEntity<List<CovidData>> findWeekOldRecords(@PathVariable("iso") final String iso,@PathVariable("date") final String date){
-        Optional<List<CovidData>> covidData = covidDataService.findWeekOldRecordsByDateAndIso(iso, date);
+    public ResponseEntity<List<CovidData>> findWeekOldRecords(
+        @ApiParam(name = "ISO code of a country", allowableValues = "CZE, SVK, GBR, DEU")
+        @PathVariable("iso") final String iso,
+        @ApiParam(name = "Upper bound of the date range") @PathVariable("date") final String date) {
+        Optional<List<CovidData>> covidData =
+            covidDataService.findWeekOldRecordsByDateAndIso(iso, date);
         covidDataService.lastYearStatistics(iso);
         return covidData.map(data -> new ResponseEntity<>(data, HttpStatus.OK))
             .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 
-    @GetMapping(path = "/data/isocodes")
-    public ResponseEntity<List<String>> isoCodes(){
+    @Operation(summary = "Get all available ISO codes")
+    @GetMapping(path = "/data/isocodes") public ResponseEntity<List<String>> isoCodes() {
         Optional<List<String>> covidData = covidDataService.fetchDistinctIsoCode();
         return covidData.map(data -> new ResponseEntity<>(data, HttpStatus.OK))
             .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 
-    @GetMapping(path = "/data/statistics/{iso}")
-    public ResponseEntity<Series> isoCodes(@PathVariable("iso") final String iso){
+    @Operation(summary = "Get last year CovidData for a specified country")
+    @GetMapping(path = "/data/statistics/{iso}") public ResponseEntity<Series> isoCodes(
+        @ApiParam(name = "ISO code of a country", allowableValues = "CZE, SVK, GBR, DEU")
+        @PathVariable("iso") final String iso) {
         Series covidData = covidDataService.lastYearStatistics(iso);
         return new ResponseEntity<Series>(covidData, HttpStatus.OK);
     }
